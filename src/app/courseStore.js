5a4944fce32ee14fdaa59fbd3dc5b99b9001bec7
Prajwal Magar <pzlmagar@gmail.com>
2023-01-26 15:17:06 +0545

    Second Commit

--- src/app/courseStore.js
+++ src/app/courseStore.js
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import {create} from 'zustand';
 
 
 import {devtools, persist} from 'zustand/middleware';
@@ -9,5 +9,28 @@ const courseStore = (set) => ({
         set((state) => ({
             courses: [course, ...state.courses],
         }))
+    },
+
+    removeCourse: (courseId) => {
+        set((state) => ({
+            courses: state.courses.filter((c) => c.id !== courseId)
+        }))
+    },
+
+    toggleCourseStatus: (courseId) => {
+        set((state) => ({
+            courses: state.courses.map((course) => course.id === courseId ? {...course, completed: !course.completed} : course)
+        }))
     }
 })
+
+
+const useCourseStore = create(
+    devtools(
+        persist(courseStore, {
+            name: "courses",
+        })
+    )
+)
+
+export default useCourseStore;
